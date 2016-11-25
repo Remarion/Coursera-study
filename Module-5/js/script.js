248af067c5224444210ed2fbcc9c9c3fe7c6600e
Oleksii <o.panchuk93@gmail.com>
2016-11-25 15:42:48 +0200

    Update script.js

--- Module-5/js/script.js
+++ Module-5/js/script.js
@@ -62,19 +62,6 @@ var switchMenuToActive = function () {
 
 // On page load (before images or CSS)
 document.addEventListener("DOMContentLoaded", function (event) {
-
-// TODO: STEP 0: Look over the code from
-// *** start ***
-// to
-// *** finish ***
-// below.
-// We changed this code to retrieve all categories from the server instead of
-// simply requesting home HTML snippet. We now also have another function
-// called buildAndShowHomeHTML that will receive all the categories from the server
-// and process them: choose random category, retrieve home HTML snippet, insert that
-// random category into the home HTML snippet, and then insert that snippet into our
-// main page (index.html).
-//
 // TODO: STEP 1: Substitute [...] below with the *value* of the function buildAndShowHomeHTML,
 // so it can be called when server responds with the categories data.
 
@@ -83,10 +70,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  function buildAndShowHomeHTML (categories) {document.querySelector("#main-content") 
-  .innerHTML = responseText;}, // ***** <---- TODO: STEP 1: Substitute [...] ******
-  true); // Explicitely setting the flag to get JSON from server processed into an object literal
-});
+  function buildAndShowHomeHTML (categories)
+	 true);
+});	
+//{document.querySelector("#main-content") 
+  //.innerHTML = responseText;}, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  // Explicitely setting the flag to get JSON from server processed into an object literal
+
 // *** finish **
 
 
